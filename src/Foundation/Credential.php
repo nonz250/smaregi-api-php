70954ea37ae8df281cdac564a53243e60918c4e0
nonz250 <hosaka.non.work@gmail.com>
2023-06-24 05:16:25 +0900

    add credential.

--- src/Foundation/Credential.php
+++ src/Foundation/Credential.php
@@ -5,4 +5,31 @@ namespace Nonz250\SmaregiApiPhp\Foundation;
 
 final class Credential
 {
+    private string $clientId;
+
+    private string $clientSecret;
+
+    public function __construct(string $clientId, string $clientSecret)
+    {
+        $this->clientId = $clientId;
+        $this->clientSecret = $clientSecret;
+    }
+
+    /**
+     * @codeCoverageIgnore
+     *
+     * @return array<string, string>
+     */
+    public function __debugInfo(): array
+    {
+        return [
+            'clientId' => $this->clientId,
+            'clientSecret' => '(secret)',
+        ];
+    }
+
+    public function authorizationBasic(): string
+    {
+        return 'Basic ' . base64_encode("$this->clientId:$this->clientSecret");
+    }
 }
