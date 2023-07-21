d0605635e0677dedbd099882064972a6c505df38
nonz250 <hosaka.non.work@gmail.com>
2023-07-22 05:35:49 +0900

    add sample php.

--- sample/public/auth.php
+++ sample/public/auth.php
@@ -4,41 +4,24 @@ declare(strict_types=1);
 require_once '../../vendor/autoload.php';
 
 use Dotenv\Dotenv;
-use Nonz250\SmaregiApiPhp\Foundation\Credential;
-use Nonz250\SmaregiApiPhp\Foundation\PsrFactories;
-use Nonz250\SmaregiApiPhp\Login\Authorize\AuthorizeRequest;
-use Nonz250\SmaregiApiPhp\Login\Client;
-use Nyholm\Psr7\Factory\Psr17Factory;
-use Psr\Http\Client\ClientExceptionInterface;
+use Nonz250\SmaregiApiPhp\Login\SmaregiProvider;
+
+session_start();
 
 (Dotenv::createImmutable(__DIR__))->load();
 
-$psr17Factory = new Psr17Factory();
-$psrFactory = new PsrFactories($psr17Factory, $psr17Factory, $psr17Factory, $psr17Factory, $psr17Factory);
-
-$client = new Client(
-    $psrFactory->getUriFactory()->createUri((string)$_ENV['SMAREGI_IDP_HOST']),
-    new Credential(
-        (string)$_ENV['SMAREGI_CLIENT_ID'],
-        (string)$_ENV['SMAREGI_CLIENT_SECRET'],
-    ),
-    new \Http\Client\Curl\Client(),
-    $psrFactory
+$provider = new SmaregiProvider(
+    (string)$_ENV['SMAREGI_IDP_HOST'],
+    (string)$_ENV['SMAREGI_CLIENT_ID'],
+    (string)$_ENV['SMAREGI_CLIENT_SECRET'],
+    'http://localhost/callback.php',
 );
 
-try {
-    $request = (new AuthorizeRequest())
-        ->withScopes(['openid', 'email', 'profile', 'offline_access']);
-    $response = $client->authorize($request);
-    var_dump($response);
-    exit();
-    //    var_dump($response->getHeader('Location'));
-    //    exit();
-    //    foreach ($response->getHeaders() as $name => $values) {
-    //        foreach ($values as $value) {
-    //            header(sprintf('%s: %s', $name, $value), false);
-    //        }
-    //    }
-    //    exit();
-} catch (JsonException|ClientExceptionInterface $e) {
+if (headers_sent()) {
+    throw new RuntimeException('Headers were already sent.');
 }
+$authorizationUrl = $provider->getAuthorizationUrl();
+$_SESSION['sampleState'] = $provider->getState();
+$_SESSION['samplePkceCode'] = $provider->getPkceCode();
+header('Location: ' . $authorizationUrl);
+exit();
--- sample/public/callback.php
+++ sample/public/callback.php
@@ -1,4 +1,66 @@
 <?php
 declare(strict_types=1);
 
-echo 'callback';
+require_once '../../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+use Nonz250\SmaregiApiPhp\Login\SmaregiProvider;
+
+session_start();
+
+(Dotenv::createImmutable(__DIR__))->load();
+
+try {
+    $error = $_GET['error'] ?? '';
+
+    if ($error) {
+        throw new RuntimeException(implode('<br>', [
+            htmlspecialchars('error: ' . ($_GET['error'] ?? '')),
+            htmlspecialchars('error_description: ' . ($_GET['error_description'] ?? '')),
+            htmlspecialchars('hint: ' . ($_GET['hint'] ?? '')),
+            htmlspecialchars('message: ' . ($_GET['message'] ?? '')),
+        ]));
+    }
+} catch (Throwable $e) {
+    echo $e->getMessage();
+    exit();
+}
+
+try {
+    $sessionState = (string)($_SESSION['sampleState'] ?? '');
+
+    if ($sessionState === '') {
+        throw new RuntimeException('Session state is empty.');
+    }
+    $sampleState = $_GET['state'] ?? '';
+
+    if ($sessionState !== $sampleState) {
+        throw new RuntimeException('Invalid state.');
+    }
+} catch (Throwable $e) {
+    echo $e->getMessage();
+    exit();
+}
+
+try {
+    $provider = new SmaregiProvider(
+        (string)$_ENV['SMAREGI_IDP_HOST'],
+        (string)$_ENV['SMAREGI_CLIENT_ID'],
+        (string)$_ENV['SMAREGI_CLIENT_SECRET'],
+        'http://localhost/callback.php',
+    );
+
+    $code = $_GET['code'] ?? '';
+
+    $provider->setPkceCode($_SESSION['samplePkceCode'] ?? '');
+    $accessToken = $provider->getAccessToken('authorization_code', [
+        'code' => $code,
+    ]);
+
+    $resourceOwner = $provider->getResourceOwner($accessToken);
+
+    echo var_export($resourceOwner->toArray(), true);
+} catch (Throwable $e) {
+    echo $e->getMessage();
+    exit();
+}
