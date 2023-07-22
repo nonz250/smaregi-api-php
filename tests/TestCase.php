6ef1e5b7280f1e90fdca8ea2c23eda73eebc6494
nonz250 <hosaka.non.work@gmail.com>
2023-07-22 09:27:37 +0900

    add test case when credential is not exists.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -11,5 +11,13 @@ abstract class TestCase extends \PHPUnit\Framework\TestCase
     {
         parent::setUp();
         (Dotenv::createImmutable(__DIR__))->load();
+
+        if (empty($_ENV['SMAREGI_IDP_HOST'])
+            || empty($_ENV['SMAREGI_CLIENT_ID'])
+            || empty($_ENV['SMAREGI_CLIENT_SECRET'])
+            || empty($_ENV['SMAREGI_CONTRACT_ID'])
+        ) {
+            $this->markTestIncomplete('クレデンシャル情報が存在しません。');
+        }
     }
 }
